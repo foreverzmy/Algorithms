c2bd8afe54d9c48cb16d47a156d88d19642fb017
MervynZ <foreverzmyer@gmail.com>
2018-01-21 01:12:02 +0800

    docs: 选择排序

--- 01 sort/selection_sort.py
+++ 01 sort/selection_sort.py
@@ -12,18 +12,17 @@ def selection_sort(arr):
     算法描述
     -------
     1. 初始状态：无序区为R[1..n]，有序区为空;
-    2. 第i趟排序 (i=1,2,3...n-1) 开始时，当前有序区和无序区分别为 R[1..i-1] 和 R(i..n）。该趟排序从当前无序区中选出关键字最小的记录 R[k]，将它与无序区的第 1 个记录 R 交换，使 R[1..i] 和 R[i+1..n) 分别变为记录个数增加 1 个的新有序区和记录个数减少 1 个的新无序区;
+    2. 第i趟排序 (i=1,2,3...n-1) 开始时，当前有序区和无序区分别为 R[1..i-1] 和 R(i..n）。该趟排序从当前无序区中找出关键字最小的记录 R[k]，将它与无序区的第 1 个记录 R 交换，使 R[1..i] 和 R[i+1..n) 分别变为记录个数增加 1 个的新有序区和记录个数减少 1 个的新无序区;
     3. n-1趟结束，数组有序化了;
     """
     length = len(arr)
     minIndex = 0
 
     for i in range(0, length - 1):
-        minIndex = i
-
+        minIndex = i  # 最小数位置标记
         for j in range(i + 1, length):
-            if arr[j] < arr[minIndex]:
-                minIndex = j
+            if arr[j] < arr[minIndex]:  # 寻找最小位置
+                minIndex = j    # 保存最小位置索引
 
         arr[i], arr[minIndex] = arr[minIndex], arr[i]
 
