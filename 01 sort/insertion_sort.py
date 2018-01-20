926e6153a9f3f873307fb8afa2f996c43d569d41
MervynZ <foreverzmyer@gmail.com>
2018-01-21 01:18:23 +0800

    chore: add utf_8

--- 01 sort/insertion_sort.py
+++ 01 sort/insertion_sort.py
@@ -1,9 +1,12 @@
+# -*- coding: utf-8 -*-
 """
 插入排序
+
+插入排序的算法描述是一种简单直观的排序算法。它的工作原理是通过构建有序序列，对于未排序数据，在已排序序列中从后向前扫描，找到相应位置并插入。插入排序在实现上，通常采用in-place排序（即只需用到 O(1) 的额外空间的排序），因而在从后向前扫描过程中，需要反复把已排序元素逐步向后挪位，为最新元素提供插入空间。
 """
 
 
-def insertion_sort_up(arr):
+def insertion_sort(arr):
     """
     从小到大排序
     """
@@ -17,22 +20,7 @@ def insertion_sort_up(arr):
     return arr
 
 
-def insertion_sort_down(arr):
-    """
-    从大到小排序
-    """
-    for j in range(1, len(arr)):
-        key = arr[j]
-        i = j - 1
-        while i >= 0 and arr[i] < key:
-            arr[i + 1] = arr[i]
-            i = i - 1
-        arr[i + 1] = key
-    return arr
-
-
-arr = [5, 2, 4, 6, 1, 3]
-result_up = insertion_sort_up(arr)
-print(result_up)
-result_down = insertion_sort_down(arr)
-print(result_down)
+if __name__ == '__main__':
+    arr = [3, 44, 38, 5, 47, 15, 36, 5, 26, 27, 2, 46, 4, 19, 50, 48]
+    result_up = insertion_sort(arr)
+    print(result_up)
