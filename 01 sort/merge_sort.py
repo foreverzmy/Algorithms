a21fd6cc4a6242389fc55e87e11892a943de9652
MervynZ <foreverzmyer@gmail.com>
2018-01-21 22:23:42 +0800

    feat: 归并排序

--- 01 sort/merge_sort.py
+++ 01 sort/merge_sort.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
 """
 归并排序
+
+归并排序是建立在归并操作上的一种有效的排序算法。该算法是采用分治法（Divide and Conquer）的一个非常典型的应用。归并排序是一种稳定的排序方法。将已有序的子序列合并，得到完全有序的序列；即先使每个子序列有序，再使子序列段间有序。若将两个有序表合并成一个有序表，称为2-路归并。
+
 """
 import math
 
 
 def merge(left, right):
+    """
+    子序列合并
+
+    说明
+    ----
+    将两个子序列合并为一个有序子序列
+    """
     result = []
     while left and right:
         if left[0] < right[0]:
@@ -23,6 +33,15 @@ def merge(left, right):
 
 
 def merge_sort(arr):
+    """
+    归并排序
+
+    算法说明
+    -------
+    1. 把长度为n的输入序列分成两个长度为n/2的子序列；
+    2. 对这两个子序列分别采用归并排序；
+    3. 将两个排序好的子序列合并成一个最终的排序序列。
+    """
     length = len(arr)
     if length < 2:
         return arr
@@ -33,7 +52,7 @@ def merge_sort(arr):
     return merge(merge_sort(left), merge_sort(right))
 
 
-arr = [8, 2, 5, 7, 4, 6, 3, 4, 1]
-
-result = merge_sort(arr)
-print(result)
+if __name__ == '__main__':
+    arr = [3, 44, 38, 5, 47, 15, 36, 5, 26, 27, 2, 46, 4, 19, 50, 48]
+    result = merge_sort(arr)
+    print(result)
